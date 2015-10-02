147c49e6a557d51ca77ecf30ebdcd51f47a03251
nitijsingh <nitijs@gmail.com>
2015-10-02 14:34:09 +0200

    Changes

--- fertliyrelation.r
+++ fertliyrelation.r
@@ -66,5 +66,22 @@ ggplot(swiss,aes(Fertility,Agriculture)) + geom_smooth()
 # There is a postive effect of percentage of males employed #
 # on the fertility rate                                     # 
 #############################################################
+source("cor.R")
+# Creating GGPlot in order to explain the corelation between fertility and army
+ggplot(swiss,aes(Fertility,Examination)) + geom_point()
 
+
+######################
+# Explaining the results
+# We observe a negative relationship between fertility and marks obtained in army
+# there we have say that more people in the army will lead to low fertility rates 
+# data:  swiss$Fertility and swiss$Examination
+#t = -5.6753, df = 45, p-value = 9.45e-07
+#alternative hypothesis: true correlation is not equal to 0
+#95 percent confidence interval:
+#  -0.7870674 -0.4403995
+# sample estimates:
+#  cor 
+# -0.6458827 
+######################
 
