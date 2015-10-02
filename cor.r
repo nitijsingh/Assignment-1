6a7aa05072acb9a3fa478e0e07d317c73a038dab
nitijsingh <nitijs@gmail.com>
2015-10-02 14:05:34 +0200

    Adding GGplot

--- cor.r
+++ cor.r
@@ -1,5 +1,5 @@
 # Creating boxplot for relation between Fertility and Percentage of males involved in Army
 library(ggplot2)
 plot (swiss$Fertility, swiss$Examination, col= "red", main ='Relation B/W Fertility and males involved in Army', xlab = 'Fertility', ylab = 'Marks')
-ggplot(swiss,aes(Examination,Fertility)) + geom_point()
+ggplot(swiss,aes(Fertility,Examination)) + geom_point()
 
